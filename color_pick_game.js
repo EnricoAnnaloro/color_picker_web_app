082b0c28d19d71167fb91d611565347e74bc6df9
enricoannaloro <enrico.annaloro@gmail.com>
2020-05-15 23:16:08 +0100

    Color Randomization Implemented

--- color_pick_game.js
+++ color_pick_game.js
@@ -1,26 +1,68 @@
-function colorClicked(){
-    console.log(this.style.backgroundColor)
-    console.log(goal_color)
-   if(this.style.backgroundColor === goal_color){
-       alert("FOund")
-   }
+function generateColors(n){
+    //returns an array arr with n colors chosed rundomly
+    let arr = [];
+
+    for (let i = 0; i < n; i++) {
+        let color = randomColor();      
+        arr[i] = color;        
+    }
+
+    return arr;
 }
 
-colors = [
-    "rgb(255, 0, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 255)",
-];
+function randomColor(){
+    //Creates a random color in RGB form "rgb(r, g, b)"
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function goalColorPicker(){
+    //Choses a random component of the array colors (chooses a random color)
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function colorClicked(){
+    //Function called when clicking on a square
+    if(this.style.backgroundColor === goal_color){
+        //Correct square selected
+        colorCorrect();
+    } else{
+        //Incorrect square selected
+        colorIncorrect(this);
+    }
+}
+
+function colorCorrect(){
+    //Function called when correct square is selected
+    message_display.textContent = "Correct"
+    top_container.style.backgroundColor = goal_color;
+
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = goal_color;
+    }
+}
+
+function colorIncorrect(square){
+    //Function called when correct square is selected
+    message_display.textContent = "Try Again";
+    square.style.backgroundColor = "black";
+}
+
+//First Initialization
+colors = generateColors(6);
+
+console.log(colors);
 
 let squares = document.querySelectorAll(".square");
-let goal_color = colors[3];
+let goal_color = goalColorPicker();
 let color_display = document.querySelector("#colorDisplay");
+let message_display = document.querySelector("#message");
+let top_container = document.querySelector("#topContainer");
 
-console.log(squares);
-
+//Applying colors and listener to squares
 for(let i = 0; i < squares.length; i++){
     //adding initial color to squares
     squares[i].style.backgroundColor = colors[i];
@@ -29,4 +71,4 @@ for(let i = 0; i < squares.length; i++){
     squares[i].addEventListener("click", colorClicked);
 }
 
-color_display.textContent = colors[3];
+color_display.textContent = goal_color;
